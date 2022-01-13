d7e19a06913e5e52f01c18fbf8b77ef26ec48d28
Daniel Rodriguez <daniel.ener.rodriguez@gmail.com>
2022-01-13 10:08:06 +0800

    Initial commit, based on ASP.net WebApp templated

---
+++ flexisource.usermanager/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace flexisource.usermanager.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
