05f6a608656627581f288707a3674c014f58655a
Daniel Rodriguez <daniel.ener.rodriguez@gmail.com>
2022-01-13 10:18:23 +0800

    Created Initial database

---
+++ flexisource.usermanager/Database/DatabaseContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace flexisource.usermanager.Database
+{
+	public class DatabaseContext : DbContext
+	{
+		public DbSet<User> Users { get; set; }
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlite("Data Source=database.db");
+		}
+	}
+
+
+	public class User
+    {
+		public int UserId { get; set; }
+		public string Name { get; set; }
+		public string Address { get; set; }
+    }
+}
+
---
+++ flexisource.usermanager/Migrations/20220113021745_InitialCreate.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace flexisource.usermanager.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Address = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
